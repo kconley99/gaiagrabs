80c31688d2b85a646b0231bcd056a81ee5b1a78d
KayleeConley <kayleefaithlove@gmail.com>
2025-05-21 10:34:28 -0700

    updating after meeting w marianne

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 let slideIndex = 0;
-showslides(slideIndex);
+let slideTimer;
+const slideDuration = 4000;
 
-function showslides() {
-  let i;
-  let slides = document.getElementsByClassName("slides");
-  for (i = 0; i < slides.length; i++) {
+function showSlides(n) {
+  const slides = document.getElementsByClassName("slides");
+
+  // Wrap around if out of bounds
+  if (n >= slides.length) slideIndex = 0;
+  if (n < 0) slideIndex = slides.length - 1;
+
+  // Hide all slides
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showslides, 2000); // Change image every 2 seconds
+
+  // Show current slide
+  slides[slideIndex].style.display = "block";
 }
+
+function nextSlide() {
+  slideIndex++;
+  showSlides(slideIndex);
+}
+
+function startSlider() {
+  nextSlide();
+  slideTimer = setTimeout(startSlider, slideDuration);
+}
+
+function plusSlides(n) {
+  clearTimeout(slideTimer);         // stop auto
+  slideIndex += n;                 // update index
+  showSlides(slideIndex);          // show correct slide
+  slideTimer = setTimeout(startSlider, slideDuration); // resume auto
+}
+
+// Start on page load
+window.onload = function () {
+  showSlides(slideIndex); // show first slide
+  slideTimer = setTimeout(startSlider, slideDuration);
+};
